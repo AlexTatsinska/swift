92103d6fb8cede31d8db403d12c7dacfbd398393
Alexandrina Tatsinska <alextatsinska@gmail.com>
2016-10-23 20:54:50 +0300

    Homework for consoleIO, conditional operators and loops. Task 1f is working only for binary numbers. Added Task 2c.

--- 3_ConsoleIO_ConditionalOperators_Loops/src/Task2c_PrintOnlyEvenNumbers.java
+++ 3_ConsoleIO_ConditionalOperators_Loops/src/Task2c_PrintOnlyEvenNumbers.java
@@ -6,23 +6,23 @@ import java.util.Scanner;
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author AlexT
  */
 public class Task2c_PrintOnlyEvenNumbers {
-    public static void main(String[] args){
+
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int number = sc.nextInt();
         int inputNumber;
-        
-        for (int i = 0; i < number; i++){
+
+        for (int i = 0; i < number; i++) {
             inputNumber = sc.nextInt();
-            if (inputNumber%2 == 0){
-                System.out.print(inputNumber+" ");
+            if (inputNumber % 2 == 0) {
+                System.out.print(inputNumber + " ");
             }
         }
     }
-    
+
 }
