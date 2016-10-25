23e452b792fd791acc9d0e469cb0efb3cc4fd9f5
Alexandrina Tatsinska <alextatsinska@gmail.com>
2016-10-25 22:07:41 +0300

    Homework for consoleIO, conditional operators and loops. Task 1f is working only for binary numbers. Added Task 2c and code format. Add Task 2b. Add Task 3a and code format. Add Task 3b.Add Task 3c. Add Task 4a.Add task 4c.

---
+++ 3_ConsoleIO_ConditionalOperators_Loops/src/Task4c_PrintEverySentence.java
@@ -0,0 +1,36 @@
+
+import java.util.Scanner;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author AlexT
+ */
+public class Task4c_PrintEverySentence {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        char[] c = str.toCharArray();
+
+        for (int i = 0; i < c.length; i++) {
+
+            switch (c[i]) {
+                case '.':
+                case '!':
+                case '?':
+                    System.out.println();
+                    break;
+                default:
+                    System.out.print(c[i]);
+                    break;
+
+            }
+        }
+    }
+
+}
