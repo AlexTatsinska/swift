a24b5e4e6b0a28c07c623bcffc46d3f00a9b163d
Alexandrina Tatsinska <alextatsinska@gmail.com>
2016-10-28 00:21:07 +0300

    All tasks work.

---
+++ 4_Arrays_Strings_SourceControl/src/Task2a_PrintLargestNumber.java
@@ -0,0 +1,32 @@
+
+import java.util.Scanner;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author AlexT
+ */
+public class Task2a_PrintLargestNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();      
+        int[] arr = new int[n];
+        int max = 0;
+        
+        for (int i=0; i<arr.length; i++){
+            arr[i] = sc.nextInt();
+        }
+        for (int i=0;i<arr.length;i++){
+            if(arr[i]>max){
+                max = arr[i];
+            }          
+        }
+        System.out.println(max);
+    }
+    
+}
