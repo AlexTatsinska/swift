800c36a45cfd09b2b9c792a8fd51fc7c5b4407b1
Alexandrina Tatsinska <alextatsinska@gmail.com>
2016-10-28 00:29:47 +0300

    All tasks work.

---
+++ 4_Arrays_Strings_SourceControl/src/Task2b_PrintSortedNumbers.java
@@ -0,0 +1,31 @@
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author AlexT
+ */
+public class Task2b_PrintSortedNumbers {
+     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();      
+        int[] arr = new int[n];
+        
+        for (int i=0; i<arr.length; i++){
+            arr[i] = sc.nextInt();
+        }
+        for (int i=0;i<arr.length;i++){
+            Arrays.sort(arr);
+            System.out.print(arr[i]+" ");
+        }
+        
+    }
+    
+}
