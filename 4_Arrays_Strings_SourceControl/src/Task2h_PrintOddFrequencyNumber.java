730a59d7918141fe4c2eadd8af962b1a7cd9fd44
Alexandrina Tatsinska <alextatsinska@gmail.com>
2016-10-30 19:37:50 +0200

    Add task 2h.

---
+++ 4_Arrays_Strings_SourceControl/src/Task2h_PrintOddFrequencyNumber.java
@@ -0,0 +1,40 @@
+
+import java.util.Scanner;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author AlexT
+ */
+public class Task2h_PrintOddFrequencyNumber {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        int check = 0;
+        int oddFrequencyNumber = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length; j++) {
+                if (arr[i] == arr[j]) {
+                    check = ++check;
+                } else {
+                    check = check + 0;
+                }
+            }
+            if (check % 2 != 0) {
+                oddFrequencyNumber = arr[i];
+            }
+            check = 0;
+        }
+        System.out.println(oddFrequencyNumber);
+    }
+}
