c1fe07d502c72d4f5fae4fc7fd2052c35bdfaeae
Alexandrina Tatsinska <alextatsinska@gmail.com>
2016-11-06 00:10:01 +0200

    Lecture 5, Task 2

--- EmployeeSalarySort/src/Employee.java
+++ EmployeeSalarySort/src/Employee.java
@@ -6,20 +6,20 @@ import java.util.Scanner;
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author AlexT
  */
 public class Employee {
+
     String name;
     float salary;
     String position;
     String department;
     byte age;
     String email;
-    
-    Employee(String name, float salary, String position, String department,byte age,String email){
+
+    Employee(String name, float salary, String position, String department, byte age, String email) {
         this.name = name;
         this.salary = salary;
         this.position = position;
@@ -27,31 +27,32 @@ public class Employee {
         this.age = age;
         this.email = email;
     }
-    Employee(String name, float salary, String position, String department){
-       this.name = name;
+
+    Employee(String name, float salary, String position, String department) {
+        this.name = name;
         this.salary = salary;
         this.position = position;
         this.department = department;
         age = -1;
-        email = "n/a";       
+        email = "n/a";
     }
-    Employee(String name, float salary, String position, String department,byte age){
-       this.name = name;
+
+    Employee(String name, float salary, String position, String department, byte age) {
+        this.name = name;
         this.salary = salary;
         this.position = position;
         this.department = department;
         this.age = age;
-        email = "n/a";       
+        email = "n/a";
     }
-    Employee(String name, float salary, String position, String department,String email){
-       this.name = name;
+
+    Employee(String name, float salary, String position, String department, String email) {
+        this.name = name;
         this.salary = salary;
         this.position = position;
         this.department = department;
         age = -1;
-        this.email = email;       
+        this.email = email;
     }
-    
-    
-   
+
 }
--- EmployeeSalarySort/src/Task2_EmployeeSalarySort.java
+++ EmployeeSalarySort/src/Task2_EmployeeSalarySort.java
@@ -1,5 +1,4 @@
 
-
 import java.util.Arrays;
 import java.util.Scanner;
 
@@ -21,11 +20,11 @@ public class Task2_EmployeeSalarySort {
         String input = null;
         Employee[] newEmployer = new Employee[n];
         float[] salaryArr = new float[n];
-       
+
         for (int i = 0; i < n; i++) {
             input = sc.nextLine();
 
-            String[] split = input.split(",");          
+            String[] split = input.split(",");
             String name = split[0].trim();
             float salary = Float.parseFloat(split[1].trim());
             String position = split[2].trim();
@@ -35,24 +34,24 @@ public class Task2_EmployeeSalarySort {
             int age = -1;
 
             Employee employee = null;
-            
+
             if (split.length == 5) {
                 if (split[4].contains("@")) {
                     email = split[4].trim();
                     employee = new Employee(name, salary, position, department, email);
                 } else {
                     age = Integer.parseInt(split[4].trim());
-                    employee = new Employee(name, salary, position, department, (byte)age);
+                    employee = new Employee(name, salary, position, department, (byte) age);
                 }
             } else if (split.length == 6) {
                 email = split[5].trim();
                 age = Integer.parseInt(split[4].trim());
-           employee = new Employee(name, salary, position, department, (byte)age, email);
-            } else if(split.length==4){
-                employee = new Employee(name, salary, position, department);          
+                employee = new Employee(name, salary, position, department, (byte) age, email);
+            } else if (split.length == 4) {
+                employee = new Employee(name, salary, position, department);
             }
-            newEmployer[i]=employee;
-            salaryArr[i]=salary;
+            newEmployer[i] = employee;
+            salaryArr[i] = salary;
             /*
             if (split.length == 6) {
                 newEmployer[i] = new Employee(split[0].trim(), Float.parseFloat(split[1].trim()), split[2].trim(), split[3].trim(), Byte.parseByte(split[4].trim()), split[5].trim());
@@ -66,24 +65,22 @@ public class Task2_EmployeeSalarySort {
             } */
         }
         Arrays.sort(salaryArr);
-     /*for (int i =0;i<salaryArr.length;i++){
+        /*for (int i =0;i<salaryArr.length;i++){
          System.out.println(salaryArr[i]);
      }*/
-     for(int i=salaryArr.length-1;i>salaryArr.length-4;i--){
-         for (int j=0;j<newEmployer.length;j++){
-             if(salaryArr[i]==newEmployer[j].salary){
-                 if(newEmployer[j].email!="n/a"){
-                     System.out.println(newEmployer[j].name+" "+newEmployer[j].department+" "+newEmployer[j].position+" "+newEmployer[j].email);
-                 } else{
-                      System.out.println(newEmployer[j].name+" "+newEmployer[j].department+" "+newEmployer[j].position);
-                 }
-             }
-         
-     }
+        for (int i = salaryArr.length - 1; i > salaryArr.length - 4; i--) {
+            for (int j = 0; j < newEmployer.length; j++) {
+                if (salaryArr[i] == newEmployer[j].salary) {
+                    if (newEmployer[j].email != "n/a") {
+                        System.out.println(newEmployer[j].name + " " + newEmployer[j].department + " " + newEmployer[j].position + " " + newEmployer[j].email);
+                    } else {
+                        System.out.println(newEmployer[j].name + " " + newEmployer[j].department + " " + newEmployer[j].position);
+                    }
+                }
 
-       
-      
+            }
+
+        }
+
     }
-
-}
 }
