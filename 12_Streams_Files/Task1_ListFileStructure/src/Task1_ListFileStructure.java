7927f4173870b45190dd8222ea3c729049f6ba57
Alexandrina Tatsinska <alextatsinska@gmail.com>
2016-12-07 22:13:21 +0200

    Lecture 12, Task 1, May be Ready

--- 12_Streams_Files/Task1_ListFileStructure/src/Task1_ListFileStructure.java
+++ 12_Streams_Files/Task1_ListFileStructure/src/Task1_ListFileStructure.java
@@ -1,32 +1,48 @@
 
 import java.io.File;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.ArrayDeque;
+import java.util.Queue;
+import java.util.Scanner;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author AlexT
  */
 public class Task1_ListFileStructure {
-    public static void main(String[] args){
-       String path = "D:\\Swift";
-       // List<String> file = new ArrayList<>();
-       File file = new File(path);
-       File[] listOfFiles = file.listFiles();
-       for (int i = 0; i < listOfFiles.length; i++) {
-      if (listOfFiles[i].isFile()) {
-        System.out.println(listOfFiles[i].getAbsolutePath());
-      } else if (listOfFiles[i].isDirectory()) {  
-       System.out.println(listOfFiles[i].getAbsolutePath());
-      }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        //String path = "D:\\Swift\\3_ConsoleIO_ConditionalOperators_Loops";
+        String path = sc.nextLine();
+        Queue<String> directory = new ArrayDeque<>();
+        File file = new File(path);
+        File[] listOfFiles = file.listFiles();
+        for (int i = 0; i < listOfFiles.length; i++) {
+            if (listOfFiles[i].isFile()) {
+                System.out.println(listOfFiles[i].getAbsolutePath());
+            } else if (listOfFiles[i].isDirectory()) {
+                directory.add(listOfFiles[i].getAbsolutePath());
+            }
+        }
+        while (!directory.isEmpty()) {
+            File subFile = new File(directory.peek());
+            File[] listOfSubFiles = subFile.listFiles();
+            for (int i = 0; i < listOfSubFiles.length; i++) {
+                if (listOfSubFiles[i].isFile()) {                   
+                    System.out.println(listOfSubFiles[i].getAbsolutePath());
+                    directory.poll();
+                } else if (listOfSubFiles[i].isDirectory()){   
+                    //System.out.println(listOfSubFiles[i].getAbsolutePath());
+                    directory.poll();
+                    directory.add(listOfSubFiles[i].getAbsolutePath());
+                    
+                }
+            }
+        }
     }
-      
-    
-       }
 }
