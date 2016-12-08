76fa71c777fb6008fbc5315796cf62b474ad33fe
Alexandrina Tatsinska <alextatsinska@gmail.com>
2016-12-08 22:31:19 +0200

    Lecture 11, Task 3, May be Ready!!

--- 11_Generics_Enums_Annotations/Task3_CardsAnnotation/src/Task3_CardsAnnotation.java
+++ 11_Generics_Enums_Annotations/Task3_CardsAnnotation/src/Task3_CardsAnnotation.java
@@ -15,30 +15,33 @@ public class Task3_CardsAnnotation {
     public static void main(String[] args) {
         Card card = new Card(CardRank.King, CardSuit.Diamonds);
 
-        //String result = card.getClass().getAnnotations().toString();
-        
-       // System.out.println(card.getClass());
-        //System.out.println(card.getRank().getClass());
-        //System.out.println(card.getSuit().getClass());
-
         for (Annotation annotation : card.getClass().getDeclaredAnnotations()) {
             String classInfo = card.getClass().toString();
             String annotationInfo = annotation.toString();
-            System.out.println(classInfo.substring(classInfo.lastIndexOf(" ")+1)+" "+classInfo.substring(classInfo.indexOf(classInfo),classInfo.lastIndexOf(" "))+" "+annotationInfo.substring(annotationInfo.lastIndexOf("=")+1, annotationInfo.indexOf(")")));
-            
+            System.out.println(classInfo.substring(classInfo.lastIndexOf(" ") + 1) + " " + classInfo.substring(classInfo.indexOf(classInfo), classInfo.lastIndexOf(" ")) + " " + annotationInfo.substring(annotationInfo.lastIndexOf("=") + 1, annotationInfo.indexOf(")")));
+
         }
         for (Annotation annotation : card.getRank().getClass().getDeclaredAnnotations()) {
             String classInfo = card.getRank().getClass().toString();
             String annotationInfo = annotation.toString();
-            System.out.println(classInfo.substring(classInfo.lastIndexOf(" ")+1)+" "+classInfo.substring(classInfo.indexOf(classInfo),classInfo.lastIndexOf(" "))+" "+annotationInfo.substring(annotationInfo.lastIndexOf("=")+1, annotationInfo.indexOf(")")));
-            
+            System.out.println(classInfo.substring(classInfo.lastIndexOf(" ") + 1) + " " + classInfo.substring(classInfo.indexOf(classInfo), classInfo.lastIndexOf(" ")) + " " + annotationInfo.substring(annotationInfo.lastIndexOf("=") + 1, annotationInfo.indexOf(")")));
+
         }
         for (Annotation annotation : card.getSuit().getClass().getDeclaredAnnotations()) {
             String classInfo = card.getSuit().getClass().toString();
             String annotationInfo = annotation.toString();
-            System.out.println(classInfo.substring(classInfo.lastIndexOf(" ")+1)+" "+classInfo.substring(classInfo.indexOf(classInfo),classInfo.lastIndexOf(" "))+" "+annotationInfo.substring(annotationInfo.lastIndexOf("=")+1, annotationInfo.indexOf(")")));
-            
+            System.out.println(classInfo.substring(classInfo.lastIndexOf(" ") + 1) + " " + classInfo.substring(classInfo.indexOf(classInfo), classInfo.lastIndexOf(" ")) + " " + annotationInfo.substring(annotationInfo.lastIndexOf("=") + 1, annotationInfo.indexOf(")")));
+
         }
+      //  printInfo(card.getClass().toString(),card.getClass().getDeclaredAnnotations().toString());
     }
+
+   // static void printInfo( String classInfo, String annotationInfo) {
+        
+    //        System.out.println(classInfo.substring(classInfo.lastIndexOf(" ") + 1) + " " + classInfo.substring(classInfo.indexOf(classInfo), classInfo.lastIndexOf(" ")) + " " + annotationInfo.substring(annotationInfo.lastIndexOf("=") + 1, annotationInfo.indexOf(")")));
+
+        
+
+  //  }
 
 }
