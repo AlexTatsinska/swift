3a408d754ab6f74d8fa2e3e63937858c93600fc6
Alexandrina Tatsinska <alextatsinska@gmail.com>
2016-12-11 17:53:42 +0200

    Lecture 16, Task 0a

---
+++ 16_Regex_Lambda_JUnit/Task0a_FindIntegers/src/Task0a_FindIntegers.java
@@ -0,0 +1,36 @@
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author AlexT
+ */
+public class Task0a_FindIntegers {
+    public static void main(String[] args){
+        String input = "123hjrt -123";
+            
+
+        String pattern ="-?\\d+";
+                        
+       printAllRegexMatches(input, pattern);
+       
+    }
+    
+    private static void printAllRegexMatches(String input, String pattern){
+        Pattern regex = Pattern.compile(pattern);
+
+        Matcher matcher = regex.matcher(input);
+
+        while (matcher.find()) {
+            System.out.printf("Found match [%s] at index [%d]%n", matcher.group(), matcher.start());
+        }
+    }
+    
+}
