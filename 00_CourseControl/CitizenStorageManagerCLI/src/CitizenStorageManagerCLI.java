ee0bedcb9e7b20ed945b1ba70e22207f34f37bb3
Alexandrina Tatsinska <alextatsinska@gmail.com>
2016-12-28 19:37:12 +0200

    Add address and can add person without current address and educations

--- 00_CourseControl/CitizenStorageManagerCLI/src/CitizenStorageManagerCLI.java
+++ 00_CourseControl/CitizenStorageManagerCLI/src/CitizenStorageManagerCLI.java
@@ -2,6 +2,10 @@
 import address.Address;
 import java.sql.SQLException;
 import java.sql.DriverManager;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import personaldetails.Citizen;
+import personaldetails.Gender;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
@@ -15,13 +19,18 @@ import java.sql.DriverManager;
 public class CitizenStorageManagerCLI {
 
     public static void main(String[] args) throws SQLException {
+        final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
         MySqlAddressStorage addr = new MySqlAddressStorage();
+        MySqlPersonStorage per = new MySqlPersonStorage();
         //final String DBMS_CONN_STRING = "jdbc:mysql://localhost:3306/citizen_registrations";
         //final String DBMS_USERNAME = "root";
         //final String DBMS_PASSWORD = "SwiftTraining1";
         // String url = DBMS_CONN_STRING+","+DBMS_USERNAME+","+DBMS_PASSWORD;
-        Address address = new Address("Bulgaria", "Sofia", "Mladost", "1766", "saharov 7", "25", 7, 111);
+        Address address = new Address("България", "Козлодуй", "Жк. 2А", "3320", " ", "77", 1, 31);
         addr.insertAddress(address);
+        //LocalDate date = LocalDate.parse("1987-06-02", formatter);
+        Citizen person = new Citizen("Петър","Ангелов","Тацински",Gender.Male,180,LocalDate.parse("1956-10-14", formatter));
+        per.insertPerson(person);
 
     }
 }
