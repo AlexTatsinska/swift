5eb323a0e45978deed3ffb30fb4d86fc55f1e56a
Alexandrina Tatsinska <alextatsinska@gmail.com>
2016-12-30 18:11:26 +0200

    Can add all information(Fixed information).

--- 00_CourseControl/DataAccessLayer/src/MySqlSocialInsuranceRecordStorage.java
+++ 00_CourseControl/DataAccessLayer/src/MySqlSocialInsuranceRecordStorage.java
@@ -1,5 +1,8 @@
 
 import insurance.SocialInsuranceRecord;
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.SQLException;
 
 /*
@@ -13,10 +16,22 @@ import java.sql.SQLException;
  * @author AlexT
  */
 public class MySqlSocialInsuranceRecordStorage implements SocialInsuranceRecordStorage {
+    static final String DBMS_CONN_STRING = "jdbc:mysql://localhost:3306/citizen_registrations";
+    static final String DBMS_USERNAME = "root";
+    static final String DBMS_PASSWORD = "SwiftTraining1";
 
     @Override
     public void insertSocialInsurance(SocialInsuranceRecord socialInsurance) throws SQLException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+       try (Connection con = DriverManager.getConnection(DBMS_CONN_STRING, DBMS_USERNAME, DBMS_PASSWORD);
+                CallableStatement statement = con.prepareCall("{call insert_social_insurance(?,?,?)}")) {
+
+            statement.setInt("year", socialInsurance.getYear());
+            statement.setInt("month", socialInsurance.getMonth());
+            statement.setDouble("amount", socialInsurance.getAmount());
+           // statement.setInt("person_id", 0);
+
+            statement.executeQuery();
+        }
     }
     
 }
