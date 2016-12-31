598864203453e02444c2f7a31fd2bde2aa6a8254
Alexandrina Tatsinska <alextatsinska@gmail.com>
2016-12-31 14:11:05 +0200

    Can add Address, Person and Education without final grade and graduated from console. Problems with cyrillic

--- 00_CourseControl/CitizenStorageManagerCLI/src/CitizenStorageManagerCLI.java
+++ 00_CourseControl/CitizenStorageManagerCLI/src/CitizenStorageManagerCLI.java
@@ -1,11 +1,6 @@
 
 import address.Address;
-import education.Education;
-import education.EducationDegree;
-import education.GradedEducation;
-import education.HigherEducation;
-import education.PrimaryEducation;
-import education.SecondaryEducation;
+import education.*;
 import insurance.SocialInsuranceRecord;
 import java.sql.SQLException;
 import java.sql.DriverManager;
@@ -33,7 +28,6 @@ public class CitizenStorageManagerCLI {
         MySqlEducationStorage addEducation = new MySqlEducationStorage();
         MySqlSocialInsuranceRecordStorage addSocialInsurance = new MySqlSocialInsuranceRecordStorage();
 
-//        Даниела;Гоцева;Христова;F;4.2.1957;202;България;София;Илинден;1012;Васил Левски;55;11;73;P;СОУ Добри Чинтулов;15.9.1963;15.6.1970;S;НПМГ;15.9.1970;30.6.1975;5.581;B;Свищовска академия;1.10.1976;1.6.1980;5.074
         Scanner sc = new Scanner(System.in, "UTF-8");
         String input = sc.nextLine();
 
@@ -81,21 +75,17 @@ public class CitizenStorageManagerCLI {
                 case "P":
                     PrimaryEducation pEducation = null;
                     institution = split[++i];
-                    // System.out.println(split[++i]);
                     enrollmentDate = LocalDate.parse(split[++i], formatter);
                     graduationDate = LocalDate.parse(split[++i], formatter);
                     pEducation = new PrimaryEducation(institution, enrollmentDate, graduationDate);
-                    //System.out.println(pEducation.getDegree().toString());
                     addEducation.insertEducation(pEducation);
                     break;
                 case "S":
                     SecondaryEducation sEducation = null;
                     institution = split[++i];
-                    // System.out.println(split[++i]);
                     enrollmentDate = LocalDate.parse(split[++i], formatter);
                     graduationDate = LocalDate.parse(split[++i], formatter);
                     sEducation = new SecondaryEducation(institution, enrollmentDate, graduationDate);
-                    //System.out.println(pEducation.getDegree().toString());
                     addEducation.insertEducation(sEducation);
                     break;
                 case "B":
@@ -111,24 +101,15 @@ public class CitizenStorageManagerCLI {
                     }
                     HigherEducation hEducation = null;
                     institution = split[++i];
-                    // System.out.println(split[++i]);
                     enrollmentDate = LocalDate.parse(split[++i], formatter);
                     graduationDate = LocalDate.parse(split[++i], formatter);
                     hEducation = new HigherEducation(institution, enrollmentDate, graduationDate, degree);
-                    //System.out.println(pEducation.getDegree().toString());
                     addEducation.insertEducation(hEducation);
                     break;
             }
         }
 
-        /* Address newAddress = new Address("България", "Козлодуй", "Жк. 2", "3320", " ", "25", 4, 15);
-        address.insertAddress(newAddress);
-        //LocalDate date = LocalDate.parse("1987-06-02", formatter);
-        Citizen newPerson = new Citizen("Стоян","Андреев","Димитров",Gender.Male,180,LocalDate.parse("1976-02-17", formatter));
-        person.insertPerson(newPerson);
-        PrimaryEducation education = new PrimaryEducation("СОУ Кирил и Методий",LocalDate.parse("1996-09-15", formatter),LocalDate.parse("2000-06-30", formatter));
-        Education newEducation = education;
-        addEducation.insertEducation(newEducation);
+        /* 
         SocialInsuranceRecord newSocialInsurance= new SocialInsuranceRecord(2015,11,322);
        // System.out.println(newSocialInsurance.getYear());
         addSocialInsurance.insertSocialInsurance(newSocialInsurance);
