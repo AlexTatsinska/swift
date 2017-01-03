77031c774f3f9006cdbd6267cc8b6a2ef40c1bec
Alexandrina Tatsinska <alextatsinska@gmail.com>
2017-01-03 16:42:44 +0200

    Delete all from database before import. Can add Address, Person, Education(without final grade and graduated) and insurance from console. Problems with cyrillic!

---
+++ 00_CourseControl/DataAccessLayer/src/sql/MySqlDeleteDatabase.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sql;
+
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author AlexT
+ */
+public class MySqlDeleteDatabase {
+    static final String DBMS_CONN_STRING = "jdbc:mysql://localhost:3306/citizen_registrations?useUnicode=true&characterEncoding=UTF-8";
+    static final String DBMS_USERNAME = "root";
+    static final String DBMS_PASSWORD = "SwiftTraining1";
+
+    
+    public void deleteDatabase() throws SQLException {
+        try (Connection con = DriverManager.getConnection(DBMS_CONN_STRING, DBMS_USERNAME, DBMS_PASSWORD);
+                CallableStatement statement = con.prepareCall("{call delete_all_citizen_registration()}")) {
+           
+            statement.executeQuery();
+        }
+    }
+    
+}
