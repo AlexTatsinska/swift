09e0ac69731b3e7b3726fbdddedd93955f152edf
Alexandrina Tatsinska <alextatsinska@gmail.com>
2017-01-07 21:41:01 +0200

    Can delete all data from database before import. Can add Address, Person, Education and insurance from console and from file.

--- 00_CourseControl/DataAccessLayer/src/interfaces/AddressStorage.java
+++ 00_CourseControl/DataAccessLayer/src/interfaces/AddressStorage.java
@@ -1,11 +1,12 @@
 package interfaces;
 
 import address.Address;
+import exception.DALException;
 import java.sql.Connection;
 import java.sql.SQLException;
 
 public interface AddressStorage {
 
-    public void insertAddress(Address address) throws SQLException;
+    public void insertAddress(Address address) throws DALException;
 
 }
