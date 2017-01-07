e5203a52841d3007eeffc10cef530b5871568418
Alexandrina Tatsinska <alextatsinska@gmail.com>
2017-01-08 00:04:14 +0200

    Can delete all data from database before import. Can add Address, Person, Education and insurance from console and from file.

--- 00_CourseControl/DataAccessLayer/src/sql/MySqlDeleteDatabaseStorage.java
+++ 00_CourseControl/DataAccessLayer/src/sql/MySqlDeleteDatabaseStorage.java
@@ -6,8 +6,6 @@ import java.sql.CallableStatement;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 public class MySqlDeleteDatabaseStorage implements DeleteDatabaseStorage {
 
