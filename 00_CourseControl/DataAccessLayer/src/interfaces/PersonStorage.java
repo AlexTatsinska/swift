f2118a34ee36c2af36a4c43a43cdeb9afb159558
Alexandrina Tatsinska <alextatsinska@gmail.com>
2017-01-15 22:13:57 +0200

    userUnfo - can surch by person_id and get result(without Educations and social insurance).

--- 00_CourseControl/DataAccessLayer/src/interfaces/PersonStorage.java
+++ 00_CourseControl/DataAccessLayer/src/interfaces/PersonStorage.java
@@ -7,5 +7,6 @@ import personaldetails.Citizen;
 public interface PersonStorage {
    
     public void insertPerson(Citizen person) throws DALException;
+    public Citizen getPresonById(int person_id) throws DALException;
 
 }
