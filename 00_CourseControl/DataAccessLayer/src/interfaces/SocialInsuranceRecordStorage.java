8ac48dfd902bafa185bccfc2f57cb97b62e924e6
Alexandrina Tatsinska <alextatsinska@gmail.com>
2017-01-20 03:29:48 +0200

    userInfo - can surch by person_id and get result. Check for social insurance access. Calculate social insurance sum.Can add social insurance and education.

--- 00_CourseControl/DataAccessLayer/src/interfaces/SocialInsuranceRecordStorage.java
+++ 00_CourseControl/DataAccessLayer/src/interfaces/SocialInsuranceRecordStorage.java
@@ -7,7 +7,7 @@ import java.util.List;
 
 public interface SocialInsuranceRecordStorage {
     
-     public void insertSocialInsurance(List <SocialInsuranceRecord> socialInsurances) throws DALException;
+     public void insertSocialInsurance(List <SocialInsuranceRecord> socialInsurances, int personId) throws DALException;
      public List<SocialInsuranceRecord> getSocialInsuranceByPersonId(int personId) throws DALException;
      public void insertSocialInsuranceFromWebPage(SocialInsuranceRecord socialInsurance, int person_id) throws DALException;
 }
