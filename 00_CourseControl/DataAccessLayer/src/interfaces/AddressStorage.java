52e61a19246c92264c035d55eb58254e7c7c09b6
Alexandrina Tatsinska <alextatsinska@gmail.com>
2017-01-20 19:56:44 +0200

    code format

--- 00_CourseControl/DataAccessLayer/src/interfaces/AddressStorage.java
+++ 00_CourseControl/DataAccessLayer/src/interfaces/AddressStorage.java
@@ -2,12 +2,11 @@ package interfaces;
 
 import address.Address;
 import exception.DALException;
-import java.sql.Connection;
-import java.sql.SQLException;
 
 public interface AddressStorage {
 
     public void insertAddress(Address address, int personId) throws DALException;
+
     public Address getAddressByPersonId(int person_id) throws DALException;
 
 }
