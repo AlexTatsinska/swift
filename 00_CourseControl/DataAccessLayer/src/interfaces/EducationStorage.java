5074e7c969a12d5e16eb9d9fc69e4b3a2d268e01
Alexandrina Tatsinska <alextatsinska@gmail.com>
2017-01-20 19:56:26 +0200

    code format

--- 00_CourseControl/DataAccessLayer/src/interfaces/EducationStorage.java
+++ 00_CourseControl/DataAccessLayer/src/interfaces/EducationStorage.java
@@ -2,12 +2,13 @@ package interfaces;
 
 import education.Education;
 import exception.DALException;
-import java.sql.SQLException;
 import java.util.List;
 
 public interface EducationStorage {
 
     public void insertEducation(List<Education> educations, int personId) throws DALException;
+
     public List<Education> getEducationsByPersonID(int personId) throws DALException;
-    public void insertEducationWebPage(Education education, int personId)throws DALException;
+
+    public void insertEducationWebPage(Education education, int personId) throws DALException;
 }
