c37456c2c93367f55a0ff1f8873123b35493ccdc
Alexandrina Tatsinska <alextatsinska@gmail.com>
2017-01-20 19:56:10 +0200

    code format

--- 00_CourseControl/DataAccessLayer/src/interfaces/SocialInsuranceRecordStorage.java
+++ 00_CourseControl/DataAccessLayer/src/interfaces/SocialInsuranceRecordStorage.java
@@ -2,12 +2,13 @@ package interfaces;
 
 import exception.DALException;
 import insurance.SocialInsuranceRecord;
-import java.sql.SQLException;
 import java.util.List;
 
 public interface SocialInsuranceRecordStorage {
-    
-     public void insertSocialInsurance(List <SocialInsuranceRecord> socialInsurances, int personId) throws DALException;
-     public List<SocialInsuranceRecord> getSocialInsuranceByPersonId(int personId) throws DALException;
-     public void insertSocialInsuranceFromWebPage(SocialInsuranceRecord socialInsurance, int person_id) throws DALException;
+
+    public void insertSocialInsurance(List<SocialInsuranceRecord> socialInsurances, int personId) throws DALException;
+
+    public List<SocialInsuranceRecord> getSocialInsuranceByPersonId(int personId) throws DALException;
+
+    public void insertSocialInsuranceFromWebPage(SocialInsuranceRecord socialInsurance, int person_id) throws DALException;
 }
