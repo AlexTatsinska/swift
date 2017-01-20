7695b0dfa1ab70194c0801b0f5b3a3edefa93776
Alexandrina Tatsinska <alextatsinska@gmail.com>
2017-01-20 19:56:18 +0200

    code format

--- 00_CourseControl/DataAccessLayer/src/interfaces/PersonStorage.java
+++ 00_CourseControl/DataAccessLayer/src/interfaces/PersonStorage.java
@@ -1,12 +1,12 @@
 package interfaces;
 
 import exception.DALException;
-import java.sql.SQLException;
 import personaldetails.Citizen;
 
 public interface PersonStorage {
-   
+
     public void insertPerson(Citizen person) throws DALException;
+
     public Citizen getPresonById(int person_id) throws DALException;
 
 }
