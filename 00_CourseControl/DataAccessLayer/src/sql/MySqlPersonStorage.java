3b5c31a299da5e48d0a54690ce57f4451c93c505
Alexandrina Tatsinska <alextatsinska@gmail.com>
2017-01-20 19:55:33 +0200

    code format

--- 00_CourseControl/DataAccessLayer/src/sql/MySqlPersonStorage.java
+++ 00_CourseControl/DataAccessLayer/src/sql/MySqlPersonStorage.java
@@ -1,6 +1,5 @@
 package sql;
 
-import address.*;
 import education.Education;
 import exception.DALException;
 import insurance.SocialInsuranceRecord;
@@ -13,34 +12,30 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import personaldetails.*;
 
 public class MySqlPersonStorage implements PersonStorage {
-    
+
     private String dbmsConnString;
     private String userName;
     private String password;
-    
+
     public MySqlPersonStorage(String dbmsConnString, String userName, String password) {
         this.dbmsConnString = dbmsConnString;
         this.userName = userName;
         this.password = password;
     }
-    
+
     @Override
     public void insertPerson(Citizen person) throws DALException {
-        AddressStorage addAddress = new MySqlAddressStorage(dbmsConnString, userName, password);        
+        AddressStorage addAddress = new MySqlAddressStorage(dbmsConnString, userName, password);
         EducationStorage addEducation = new MySqlEducationStorage(dbmsConnString, userName, password);
         SocialInsuranceRecordStorage addSocialInsurance = new MySqlSocialInsuranceRecordStorage(dbmsConnString, userName, password);
-        DeleteDatabaseStorage deleteDatabase = new MySqlDeleteDatabaseStorage(dbmsConnString, userName, password); 
-        
+        DeleteDatabaseStorage deleteDatabase = new MySqlDeleteDatabaseStorage(dbmsConnString, userName, password);
+
         try (Connection con = DriverManager.getConnection(dbmsConnString, userName, password);
                 CallableStatement statement = con.prepareCall("{call insert_person(?,?,?,?,?,?,?)}")) {
-            
+
             statement.setString("first_name", person.getFirstName());
             statement.setString("middle_name", person.getMiddleName());
             statement.setString("last_name", person.getLastName());
@@ -48,16 +43,16 @@ public class MySqlPersonStorage implements PersonStorage {
             statement.setInt("height", person.getHeight());
             statement.setDate("birth_date", (Date.valueOf(person.getDateOfBirth())));
             statement.executeQuery();
-            int personId = statement.getInt("person_id");            
+            int personId = statement.getInt("person_id");
             addAddress.insertAddress(person.getAddress(), personId);
             addEducation.insertEducation(person.getEducations(), personId);
             addSocialInsurance.insertSocialInsurance(person.getSocialInsuranceRecords(), personId);
-                    
+
         } catch (SQLException ex) {
             throw new DALException("Error during person import!", ex);
         }
     }
-    
+
     @Override
     public Citizen getPresonById(int person_id) throws DALException {
         Citizen person = null;
@@ -77,7 +72,7 @@ public class MySqlPersonStorage implements PersonStorage {
         try (Connection conn = DriverManager.getConnection(dbmsConnString, userName, password);
                 PreparedStatement statement = conn.prepareStatement(sql)) {
             statement.setInt(1, person_id);
-            
+
             try (ResultSet rs = statement.executeQuery()) {
                 while (rs.next()) {
                     String firstName = rs.getString("pe.first_name");
@@ -91,16 +86,16 @@ public class MySqlPersonStorage implements PersonStorage {
                     } else {
                         person = new Citizen(firstName, middleName, lastName, Gender.Male, height, dateOfBirth);
                     }
-                    AddressStorage address = new MySqlAddressStorage(dbmsConnString, userName, password);                    
+                    AddressStorage address = new MySqlAddressStorage(dbmsConnString, userName, password);
                     EducationStorage educations = new MySqlEducationStorage(dbmsConnString, userName, password);
                     SocialInsuranceRecordStorage socialInsurances = new MySqlSocialInsuranceRecordStorage(dbmsConnString, userName, password);
                     person.setAddress(address.getAddressByPersonId(person_id));
                     for (Education education : educations.getEducationsByPersonID(person_id)) {
-                        person.addEducation(education);                        
+                        person.addEducation(education);
                     }
                     for (SocialInsuranceRecord socialInsurance : socialInsurances.getSocialInsuranceByPersonId(person_id)) {
                         person.addSocialInsuranceRecord(socialInsurance);
-                    }                    
+                    }
                 }
             }
         } catch (SQLException ex) {
