15435bc9045c7d861291684b18851324a5393203
Alexandrina Tatsinska <alextatsinska@gmail.com>
2017-01-27 15:19:58 +0200

    Lecture 4, Task 2b, Fixed

--- 4_Arrays_Strings_SourceControl/src/Task2b_PrintSortedNumbers.java
+++ 4_Arrays_Strings_SourceControl/src/Task2b_PrintSortedNumbers.java
@@ -7,25 +7,27 @@ import java.util.Scanner;
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author AlexT
  */
 public class Task2b_PrintSortedNumbers {
-     public static void main(String[] args) {
+
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();      
+        int n = sc.nextInt();
         int[] arr = new int[n];
-        
-        for (int i=0; i<arr.length; i++){
+
+        for (int i = 0; i < arr.length; i++) {
             arr[i] = sc.nextInt();
         }
-        for (int i=0;i<arr.length;i++){
-            Arrays.sort(arr);
-            System.out.print(arr[i]+" ");
-        }
         
+        Arrays.sort(arr);
+        
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+
     }
-    
+
 }
